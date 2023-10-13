6fe0ba8eaf9e6aaf4b7d65da630276107eed643d
Priyam Gautam <98340870+Priyam117@users.noreply.github.com>
2023-10-13 22:55:19 +0530

    lecture 4 complete

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", { id: "title", }, "Heading 1 from parcel");
-const heading2 = React.createElement("h2", { id: "title2", }, "Heading 2");
+const Heading = (
+  <h1 key="hllo">Hello, world!</h1>
+);
 
-const container = React.createElement("div", {}, [heading1, heading2]);
+const Title = () => {
+  return (
+  <div>
+    <h2>I am from another component</h2>
+  </div>
+  );
+}
+
+// Component Compozation Component inside a component
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      {Heading}
+    <Title/>
+      <h1>Header functional Component</h1>
+      <h2>This is h2 element</h2>
+
+    </div>
+  );
+}
+// const HeaderComponent2 = function () {
+//   <div>
+//     <h1>Header functional Component</h1>
+//     <h2>This is h2 element</h2>
+//   </div>
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+// root.render(Heading);
+root.render(<HeaderComponent/>);
