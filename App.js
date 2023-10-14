4c9dfbeabb817cc80902c024ad6edabd0b2fa48d
Priyam Gautam <98340870+Priyam117@users.noreply.github.com>
2023-10-14 23:45:49 +0530

    starting our first react App

--- App.js
+++ App.js
@@ -1,39 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Heading = (
-  <h1 key="hllo">Hello, world!</h1>
-);
+
 
 const Title = () => {
   return (
-  <div>
-    <h2>I am from another component</h2>
-  </div>
+    <a href="/">
+    <h1 className="logo">Food Villa</h1>
+    </a>
   );
 }
 
-// Component Compozation Component inside a component
 
 const HeaderComponent = () => {
   return (
-    <div>
-      {Heading}
+    <div className="header">
     <Title/>
-      <h1>Header functional Component</h1>
-      <h2>This is h2 element</h2>
-
+    <div className="nav-item">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
     </div>
   );
 }
-// const HeaderComponent2 = function () {
-//   <div>
-//     <h1>Header functional Component</h1>
-//     <h2>This is h2 element</h2>
-//   </div>
+
+// const AppLayout = () => {
+//   return (
+
+//       <HeaderComponent/>
+//       <Body/>
+//       <Footer />
+
+//   );
 // }
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(Heading);
+
 root.render(<HeaderComponent/>);
